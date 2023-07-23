66b9dd419c94239aa5caba775bfe31dd6159830e
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2023-07-23 11:07:28 +0000

    add gpu.rs

--- rtm-lib/src/gpu.rs
+++ rtm-lib/src/gpu.rs
@@ -1,6 +1,8 @@
 use crate::arg::*;
 use crate::common::{ mkdir, cat_id, record_current_dir, cd, run, rmdir };
 use std::process::Command;
+use std::fs::File;
+use std::io::Write;
 
 pub fn execute(dir_name: &str, message: &str, gpunum: usize, gpusupport: &GpuSupport, time: usize) {
 
@@ -12,8 +14,17 @@ pub fn execute(dir_name: &str, message: &str, gpunum: usize, gpusupport: &GpuSup
 
     // cat
     cat_id(dir_name);
-    cat_toml(dir_name);
-    cat_gpu_code();
+    cat_toml(dir_name, message);
+    cat_gpu_code(dir_name);
+
+    match gpusupport {
+        GpuSupport::Opencl => {
+            cat_cl(dir_name);
+        }
+        GpuSupport::Cuda => {
+            cat_fatbin(dir_name);
+        }
+    }
 
     // cd
     cd(dir_name);
@@ -38,8 +49,10 @@ pub fn execute(dir_name: &str, message: &str, gpunum: usize, gpusupport: &GpuSup
 }
 
 
-fn cat_toml(dir_name: &str) {
-
+fn cat_toml(dir_name: &str, message: &str) {
+    let mut file = File::create(format!("{}/{}", dir_name, "Cargo.toml")).unwrap();
+    file.write_all(format!("[package]\nname=\"{}\"\nversion=\"0.1.0\"", message).as_bytes()).unwrap();
+    file.write_all(format!("[dependencies]\nrust-gpu-tools = { git = \"https://github.com/filecoin-project/rust-gpu-tools\", rev=\"807fc6c7353b23b9bbeffaafc29e497374423d5d\" }").as_bytes()).unwrap();
 }
 
 fn cat_gpu_code(dir_name: &str, ) {
